31ed4307cfca1e1b6ba3e939da686042096b7b4a
Kevin Kim <kevinim613@gmail.com>
2025-10-22 20:28:49 -0700

    kevin misc

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -204,6 +204,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- Kevin
+vim.keymap.set('n', ';', ':', { noremap = true, desc = 'Enter command mode' })
+vim.keymap.set('v', ';', ':', { noremap = true, desc = 'Enter command mode' })
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Enter command mode' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
